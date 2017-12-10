1595ffaa9d4f92fc90b55cb912314f43ca9eaa10
Nathan Sobo <nathan@github.com>
2017-12-10 07:40:02 -0700

    Add a cursor
    Unlike the iterator, the cursor's next method provides the summary at
    the start of the element it yields. This summary is borrowed from the
    cursor, which isn't possible to implement for the iterator trait.
    
    The cursor can also be used to build a prefix tree up to a specific
    dimension. After building the tree, the cursor is parked on the element
    following this prefix.

--- src/tree.rs
+++ src/tree.rs
@@ -51,6 +51,13 @@ pub struct Iter<'a, T: 'a + Item> {
     stack: Vec<(&'a Tree<T>, usize)>,
 }
 
+struct Cursor<'a, T: 'a + Item> {
+    tree: &'a Tree<T>,
+    started: bool,
+    stack: Vec<(&'a Tree<T>, usize)>,
+    summary: T::Summary
+}
+
 impl<T: Item> From<T> for Tree<T> {
     fn from(value: T) -> Self {
         Tree(Arc::new(Node::Leaf {
@@ -90,6 +97,10 @@ impl<'a, T: Item> Tree<T> {
 
     fn iter(&self) -> Iter<T> {
         Iter::new(self)
+    }
+
+    fn cursor(&self) -> Cursor<T> {
+        Cursor::new(self)
     }
 
     fn len<D: Dimension<Summary=T::Summary>>(&self) -> D {
@@ -206,29 +217,6 @@ impl<'a, T: Item> Tree<T> {
                     }
                 }
             }
-
-        }
-    }
-
-    pub fn get<D: Dimension<Summary=T::Summary>>(&self, target: D) -> Option<&T> {
-        self.get_internal(D::default(), target)
-    }
-
-    fn get_internal<D: Dimension<Summary=T::Summary>>(&self, mut current_pos: D, target: D) -> Option<&T> {
-        match self.0.as_ref() {
-            &Node::Empty => None,
-            &Node::Leaf {ref value, ..} => Some(value),
-            &Node::Internal {ref children, ..} => {
-                for ref child in children {
-                    let next_pos = current_pos.accumulate(&D::from_summary(&child.summary()));
-                    if next_pos > target {
-                        return child.get_internal(current_pos, target);
-                    } else {
-                        current_pos = next_pos;
-                    }
-                }
-                None
-            }
         }
     }
 
@@ -339,33 +327,156 @@ impl<'a, T: 'a + Item> Iterator for Iter<'a, T> where Self: 'a {
     }
 }
 
+impl<'tree, T: 'tree + Item> Cursor<'tree, T> {
+    fn new(tree: &'tree Tree<T>) -> Self {
+        Self {
+            tree,
+            started: false,
+            stack: Vec::with_capacity(tree.height() as usize),
+            summary: T::Summary::default()
+        }
+    }
+
+    pub fn next(&mut self) -> Option<(&'tree T, &T::Summary)> {
+        if self.started {
+            while self.stack.len() > 0 {
+                let (prev_subtree, index) = {
+                    let &mut (prev_subtree, ref mut index) = self.stack.last_mut().unwrap();
+                    if prev_subtree.height() == 1 {
+                        self.summary.accumulate(prev_subtree.children()[*index].summary());
+                    }
+                    *index += 1;
+                    (prev_subtree, *index)
+                };
+                if let Some(child) = prev_subtree.children().get(index) {
+                    return self.descend_to_first_item(child);
+                } else {
+                    self.stack.pop();
+                }
+            }
+            None
+        } else {
+            self.started = true;
+            if let Some(&(subtree, index)) = self.stack.last() {
+                Some((&subtree.children()[index].value(), &self.summary))
+            } else {
+                match self.tree.0.as_ref() {
+                    &Node::Leaf { ref value, .. } => {
+                        Some((value, &self.summary))
+                    },
+                    _ => None
+                }
+            }
+        }
+    }
+
+    pub fn build_prefix<D: Dimension<Summary=T::Summary>>(&mut self, end: &D) -> Tree<T> {
+        // self.reset();
+
+        let mut prefix = Tree::new();
+        let mut subtree = self.tree;
+
+        loop {
+            match subtree.0.as_ref() {
+                &Node::Internal {ref summary, ref children, ..} => {
+                    let subtree_start = D::from_summary(&self.summary);
+                    let subtree_end = subtree_start.accumulate(&D::from_summary(summary));
+                    if *end >= subtree_end {
+                        self.summary.accumulate(summary);
+                        prefix.push(subtree.clone());
+                        break;
+                    } else {
+                        let mut child_start = subtree_start.clone();
+                        for (index, child) in children.iter().enumerate() {
+                            let child_end = child_start.accumulate(&D::from_summary(child.summary()));
+                            if *end >= child_end {
+                                self.summary.accumulate(child.summary());
+                                prefix.push(child.clone());
+                                child_start = child_end;
+                            } else {
+                                self.stack.push((subtree, index));
+                                subtree = child;
+                                break;
+                            }
+                        }
+                    }
+                },
+                _ => break
+            }
+        }
+
+        prefix
+    }
+
+    fn descend_to_first_item<'a>(&'a mut self, mut tree: &'tree Tree<T>) -> Option<(&'tree T, &'a T::Summary)> {
+        loop {
+            match tree.0.as_ref() {
+                &Node::Empty => return None,
+                &Node::Leaf {ref value, ..} => return Some((value, &self.summary)),
+                &Node::Internal { ref children, ..} => {
+                    self.stack.push((tree, 0));
+                    tree = &children[0];
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[derive(Default, Eq, PartialEq, Clone, Debug)]
+    pub struct IntegersSummary {
+        count: usize,
+        sum: usize
+    }
+
+    #[derive(Ord, PartialOrd, Default, Eq, PartialEq, Clone, Debug)]
+    struct Count(usize);
+
+    #[derive(Ord, PartialOrd, Default, Eq, PartialEq, Clone, Debug)]
+    struct Sum(usize);
+
     impl Item for u16 {
-        type Summary = usize;
+        type Summary = IntegersSummary;
 
-        fn summarize(&self) -> usize {
-            1
+        fn summarize(&self) -> Self::Summary {
+            IntegersSummary {
+                count: 1,
+                sum: *self as usize
+            }
         }
     }
 
-    impl Summary for usize {
+    impl Summary for IntegersSummary {
         fn accumulate(&mut self, other: &Self) {
-            *self += *other;
+            self.count += other.count;
+            self.sum += other.sum;
         }
     }
 
-    impl Dimension for usize {
-        type Summary = usize;
+    impl Dimension for Count {
+        type Summary = IntegersSummary;
 
         fn from_summary(summary: &Self::Summary) -> Self {
-            *summary
+            Count(summary.count)
         }
 
         fn accumulate(&self, other: &Self) -> Self {
-            *self + *other
+            Count(self.0 + other.0)
+        }
+    }
+
+    impl Dimension for Sum {
+        type Summary = IntegersSummary;
+
+        fn from_summary(summary: &Self::Summary) -> Self {
+            Sum(summary.sum)
+        }
+
+        fn accumulate(&self, other: &Self) -> Self {
+            Sum(self.0 + other.0)
         }
     }
 
@@ -395,7 +506,7 @@ mod tests {
     fn splice() {
         let mut tree = Tree::new();
         tree.extend(0..10);
-        tree.splice(&2..&8, 20..23);
+        tree.splice(&Count(2)..&Count(8), 20..23);
         assert_eq!(
             tree.items(),
             vec![0, 1, 20, 21, 22, 8, 9]
@@ -411,14 +522,14 @@ mod tests {
         let count = rng.gen_range(0, 100);
         tree.extend(rng.gen_iter().take(count));
 
-        for i in 0..100 {
-            let end = rng.gen_range(0, tree.len::<usize>() + 1);
+        for _i in 0..100 {
+            let end = rng.gen_range(0, tree.len::<Count>().0 + 1);
             let start = rng.gen_range(0, end + 1);
             let count = rng.gen_range(0, 100);
             let new_items = rng.gen_iter().take(count).collect::<Vec<u16>>();
             let mut original_tree_items = tree.items();
 
-            tree.splice(&start..&end, new_items.clone());
+            tree.splice(&Count(start)..&Count(end), new_items.clone());
             original_tree_items.splice(start..end, new_items);
 
             assert_eq!(tree.items(), original_tree_items);
@@ -426,13 +537,30 @@ mod tests {
     }
 
     #[test]
-    fn get() {
+    fn cursor() {
+        let tree = Tree::<u16>::new();
+        let mut cursor = tree.cursor();
+        assert_eq!(cursor.build_prefix(&Sum(0)), Tree::new());
+        assert_eq!(cursor.next(), None);
+
+        let mut tree = Tree::<u16>::new();
+        tree.extend(vec![1]);
+        let mut cursor = tree.cursor();
+        assert_eq!(cursor.build_prefix(&Sum(0)), Tree::new());
+        assert_eq!(cursor.next(), Some((&1, &IntegersSummary {count: 0, sum: 0})));
+
         let mut tree = Tree::new();
-        tree.extend((1..8));
-        assert_eq!(*tree.get(0).unwrap(), 1);
-        assert_eq!(*tree.get(2).unwrap(), 3);
-        assert_eq!(*tree.get(4).unwrap(), 5);
-        assert_eq!(*tree.get(6).unwrap(), 7);
-        assert_eq!(tree.get(7), None);
+        tree.extend(vec![1, 2, 3, 4, 5, 6]);
+        let mut cursor = tree.cursor();
+        assert_eq!(cursor.build_prefix(&Sum(4)).items(), [1, 2]);
+        assert_eq!(cursor.next(), Some((&3, &IntegersSummary {count: 2, sum: 3})));
+        assert_eq!(cursor.next(), Some((&4, &IntegersSummary {count: 3, sum: 6})));
+        assert_eq!(cursor.next(), Some((&5, &IntegersSummary {count: 4, sum: 10})));
+        assert_eq!(cursor.next(), Some((&6, &IntegersSummary {count: 5, sum: 15})));
+        assert_eq!(cursor.next(), None);
+
+        let mut cursor = tree.cursor();
+        assert_eq!(cursor.build_prefix(&tree.len::<Sum>()).items(), tree.items());
+        assert_eq!(cursor.next(), None);
     }
 }
