b7fb8e91afabede17ca5ef2dda180700c60aa6ee
Nathan Sobo <nathan@github.com>
2017-12-21 13:48:58 -0700

    Allow property descriptors to be passed to Env::define_class

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 extern crate napi;
 extern crate proton_core;
 
-use napi::{sys, Env, Result, Value, Object};
+use napi::{sys, Result, Env, Value, Object};
 
 register_module!(proton, init);
 
@@ -13,35 +13,22 @@ fn init<'env>(env: &'env Env, exports: &'env mut Object) -> Result<Option<Object
 
 mod buffer {
     use proton_core::{Buffer, ReplicaId};
-    use napi::{Env, Value, Function, Result};
+    use napi::{Env, Property, Value, Function, Result};
 
     pub fn init(env: &Env) -> Function {
-        env.define_class("TextBuffer", callback!(constructor))
+        env.define_class("TextBuffer", callback!(constructor), vec![
+            Property::new("length").with_getter(callback!(get_length))
+        ])
     }
 
     fn constructor<'a>(env: &'a Env, mut this: Value, args: &[Value<'a>]) -> Result<Option<Value<'a>>> {
-       let replica_id: ReplicaId = args[0].into_number()?.into();
-       env.wrap(&mut this, Buffer::new(replica_id))?;
-       Ok(None)
-   }
-}
+        let replica_id: ReplicaId = args[0].into_number()?.into();
+        env.wrap(&mut this, Buffer::new(replica_id))?;
+        Ok(None)
+    }
 
-// let env = napi::Env::from(env);
-//
-// let text_buffer_constructor = env.define_class(
-//     "TextBuffer",
-//     env.create_function("TextBuffer", 1, |this: napi::Value, args: Vec<napi::Value>| {
-//         let replica_id = args[0].try_into::<napi::Number>().unwrap().value_int64() as ReplicaId;
-//         env.wrap_object(this, Buffer::new(replica_id)).unwrap();
-//     }).unwrap(),
-//     [
-//         napi::PropertyDescriptor::new("length").with_getter(|this: napi::Value| {
-//             let buffer = env.unwrap_object().unwrap();
-//             env.create_int64(buffer.len())
-//         })
-//     ]
-// );
-//
-// let exports = napi::Object::try_from(&env, exports).unwrap();
-// exports.set_named_property("TextBuffer", text_buffer_constructor).unwrap();
-// exports.into()
+    fn get_length<'a>(env: &'a Env, this: Value, _args: &[Value]) -> Result<Option<Value<'a>>> {
+       let buffer: &Buffer = unsafe { env.unwrap(&this)? };
+       Ok(Some(env.create_int64(buffer.len() as i64).into()))
+    }
+}
