e2aa8435269ff6e0a6e68aeb361aa8e669a24740
Nathan Sobo <nathan@github.com>
2017-12-31 16:08:03 -0700

    Refactor in preparation to handle task termination

--- napi/src/async.rs
+++ napi/src/async.rs
@@ -7,25 +7,31 @@ use std::ptr;
 use std::os::raw::c_void;
 use super::sys;
 
-struct UvHandle(*mut sys::uv_async_t);
-
 struct Task<T: 'static + Future> {
-    notify_handle: Arc<TaskNotifyHandle>,
-    spawn: Spawn<T>
+    spawn: Spawn<T>,
+    notify_handle: Arc<TaskNotifyHandle>
 }
 
-struct TaskNotifyHandle(RwLock<Option<UvHandle>>);
+struct TaskNotifyHandle(RwLock<Option<UvAsyncHandle>>);
 
-unsafe impl Send for UvHandle {}
-unsafe impl Sync for UvHandle {}
+struct UvAsyncHandle(Box<sys::uv_async_t>);
+
+unsafe impl Send for UvAsyncHandle {}
+unsafe impl Sync for UvAsyncHandle {}
 
 impl<T: 'static + Future> Task<T> {
     fn poll_future(&mut self) -> bool {
         match self.spawn.poll_future_notify(&self.notify_handle, 0) {
-            Ok(Async::Ready(_)) => false,
-            Ok(Async::NotReady) => true,
+            Ok(Async::Ready(_)) => true,
+            Ok(Async::NotReady) => false,
             Err(_) => panic!("Future yielded an error")
         }
+    }
+}
+
+impl TaskNotifyHandle {
+    fn new(event_loop: *mut sys::uv_loop_t, callback: sys::uv_async_cb, data: *mut c_void) -> Self {
+        TaskNotifyHandle(RwLock::new(Some(UvAsyncHandle::new(event_loop, callback, data))))
     }
 }
 
@@ -33,15 +39,27 @@ impl Notify for TaskNotifyHandle {
     fn notify(&self, _id: usize) {
         if let Some(ref uv_handle) = *self.0.read().unwrap() {
             unsafe {
-                sys::uv_async_send(uv_handle.0);
+                sys::uv_async_send(mem::transmute_copy(&uv_handle.0));
             }
+        }
+    }
+}
+
+impl UvAsyncHandle {
+    fn new(event_loop: *mut sys::uv_loop_t, callback: sys::uv_async_cb, data: *mut c_void) -> Self {
+        unsafe {
+            let mut handle = UvAsyncHandle(Box::new(mem::uninitialized()));
+            let status = sys::uv_async_init(event_loop, mem::transmute_copy(&handle.0), callback);
+            assert!(status == 0, "Non-zero status returned from uv_async_init");
+            handle.0.data = data;
+            handle
         }
     }
 }
 
 extern "C" fn poll_future_on_main_thread<T: 'static + Future>(handle: *mut sys::uv_async_t) {
     let mut task: Box<Task<T>> = unsafe { Box::from_raw((*handle).data as *mut Task<T>) };
-    if task.poll_future() {
+    if !task.poll_future() {
         mem::forget(task); // Don't drop task if it isn't complete.
     }
 }
@@ -50,19 +68,19 @@ pub fn spawn<T: 'static + Future>(future: T, event_loop: *mut sys::uv_loop_t) {
     let spawn = executor::spawn(future);
 
     unsafe {
-        let task_ptr = Box::into_raw(Box::new(Task {
-            notify_handle: mem::uninitialized(),
-            spawn
-        }));
+        let mut task = Box::new(Task {
+            spawn,
+            notify_handle: mem::uninitialized()
+        });
 
-        let raw_uv_handle: *mut sys::uv_async_t = Box::into_raw(Box::new(mem::uninitialized()));
-        let status = sys::uv_async_init(event_loop, raw_uv_handle, Some(poll_future_on_main_thread::<T>));
-        assert!(status == 0, "Non-zero status returned from uv_async_init");
-        (*raw_uv_handle).data = task_ptr as *mut c_void;
-        ptr::write(&mut (*task_ptr).notify_handle, Arc::new(TaskNotifyHandle(RwLock::new(Some(UvHandle(raw_uv_handle))))));
+        ptr::write(&mut task.notify_handle, Arc::new(TaskNotifyHandle::new(
+            event_loop,
+            Some(poll_future_on_main_thread::<T>),
+            mem::transmute_copy(&task)
+        )));
 
-        if !(*task_ptr).poll_future() {
-            Box::from_raw(task_ptr); // Drop task if it is complete.
+        if !task.poll_future() {
+            mem::forget(task)
         }
     }
 }
