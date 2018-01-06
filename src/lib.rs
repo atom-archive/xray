295fbd6864be2ec5ea8155e0a0843570929605ea
Nathan Sobo <nathan@github.com>
2018-01-05 22:12:33 -0700

    Rename to Xray

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 #[macro_use]
 extern crate collider;
-extern crate proton_core;
+extern crate xray_core;
 
 use std::rc::Rc;
 use std::cell::RefCell;
-use proton_core::{Buffer, ReplicaId, Editor};
+use xray_core::{Buffer, ReplicaId, Editor};
 use collider::{Result, Env, Property, Value, Function, Object};
 
-register_module!(proton, init);
+register_module!(xray, init);
 
 fn init<'env>(env: &'env Env, exports: &'env mut Object) -> Result<Option<Object<'env>>> {
     exports.set_named_property("TextBuffer", buffer::init(env))?;
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {TextBuffer, TextEditor} = require('./target/debug/proton.node')
+const {TextBuffer, TextEditor} = require('./target/debug/xray.node')
 
 const buffer = new TextBuffer(1)
 buffer.splice(0, 0, 'Hello, world!')
