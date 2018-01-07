ee024db17b5886ee2e67ff8fa3919387675716f7
Nathan Sobo <nathan@github.com>
2018-01-06 21:05:14 -0700

    Fix moduleName

--- xray_core_node/scripts/build.js
+++ xray_core_node/scripts/build.js
@@ -6,7 +6,7 @@ const path = require('path')
 const buildDebug = ['-d', '--debug'].includes(process.argv[2])
 const releaseFlag = buildDebug ? '' : '--release '
 const targetDir = buildDebug ? 'debug' : 'release'
-const moduleName = 'xray_core';
+const moduleName = 'xray';
 
 cp.execSync(`cargo rustc ${releaseFlag}--verbose -- -Clink-args=\"-undefined dynamic_lookup -export_dynamic\"`, {stdio: 'inherit'})
 cp.execSync(`cp target/${targetDir}/{lib${moduleName}.dylib,${moduleName}.node}`, {stdio: 'inherit'})
