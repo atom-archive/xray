1739050e70d3eceee90e2e2cbfe1da713e5807c4
Nathan Sobo <nathan@github.com>
2018-01-29 11:20:49 -0700

    WIP

--- xray_electron/lib/render_process/text_editor/shaders.js
+++ xray_electron/lib/render_process/text_editor/shaders.js
@@ -1,18 +1,29 @@
+exports.attributes = {
+  unitQuadVertex: 0,
+  targetOrigin: 1,
+  targetSize: 2,
+  textColorRGBA: 3,
+  atlasOrigin: 4,
+  atlasSize: 5
+}
+
 exports.textBlendVertex = `
-  attribute vec2 unitQuadCorner;
-  attribute vec2 targetOrigin;
-  attribute vec2 targetSize;
-  attribute vec4 textColorRGBA;
-  attribute vec2 atlasOrigin;
-  attribute vec2 atlasSize;
+  #version 300 es
 
-  uniform vec2 posScale;
+  layout (location = 0) in vec2 unitQuadVertex;
+  layout (location = 1) in vec2 targetOrigin;
+  layout (location = 2) in vec2 targetSize;
+  layout (location = 3) in vec4 textColorRGBA;
+  layout (location = 4) in vec2 atlasOrigin;
+  layout (location = 5) in vec2 atlasSize;
 
-  varying vec4 textColor;
-  varying vec2 atlasPosition;
+  // uniform vec2 scale;
+
+  flat out vec4 textColor;
+  out vec2 atlasPosition;
 
   void main() {
-      vec2 targetPixelPosition = targetOrigin + unitQuadCorner * targetSize;
+      vec2 targetPixelPosition = targetOrigin + unitQuadVertex * targetSize;
       // vec2 pos = pixelPos * posScale + vec2(-1.0, 1.0);
       gl_Position = vec4(targetPixelPosition, 0.0, 1.0);
       textColor = textColorRGBA * vec4(1.0 / 255.0);
@@ -20,37 +31,43 @@ exports.textBlendVertex = `
       textColor = textColor * textColor;
       atlasPosition = atlasOrigin + targetPixelPosition * atlasSize;
   }
-`
+`.trim()
 
 exports.textBlendPass1Fragment = `
+  #version 300 es
+
   precision mediump float;
 
-  varying vec4 textColor;
-  varying vec2 atlasPosition;
+  layout(location = 0) out vec4 outColor;
+  flat in vec4 textColor;
+  in vec2 atlasPosition;
 
   uniform sampler2D atlasTexture;
 
   void main() {
-    vec3 atlasColor = texture2D(atlasTexture, atlasPosition).rgb;
+    vec3 atlasColor = texture(atlasTexture, atlasPosition).rgb;
     vec3 textColorRGB = textColor.rgb;
     vec3 correctedAtlasColor = mix(vec3(1.0) - atlasColor, sqrt(vec3(1.0) - atlasColor * atlasColor), textColorRGB);
-    gl_FragColor = vec4(correctedAtlasColor, 1.0);
+    outColor = vec4(correctedAtlasColor, 1.0);
   }
-`
+`.trim()
 
 exports.textBlendPass2Fragment = `
+  #version 300 es
+
   precision mediump float;
 
-  varying vec4 textColor;
-  varying vec2 atlasPosition;
+  layout(location = 0) out vec4 outColor;
+  flat in vec4 textColor;
+  in vec2 atlasPosition;
 
   uniform sampler2D atlasTexture;
 
   void main() {
-    vec3 atlasColor = texture2D(atlasTexture, atlasPosition).rgb;
+    vec3 atlasColor = texture(atlasTexture, atlasPosition).rgb;
     vec3 textColorRGB = textColor.rgb;
     vec3 correctedAtlasColor = mix(vec3(1.0) - atlasColor, sqrt(vec3(1.0) - atlasColor * atlasColor), textColorRGB);
     vec3 adjustedForegroundColor = textColorRGB * correctedAtlasColor;
-    gl_FragColor = vec4(adjustedForegroundColor, 1.0);
+    outColor = vec4(adjustedForegroundColor, 1.0);
   }
-`
+`.trim()
--- xray_electron/lib/render_process/text_editor/text_plane.js
+++ xray_electron/lib/render_process/text_editor/text_plane.js
@@ -3,11 +3,9 @@ const PropTypes = require("prop-types");
 const { styled } = require("styletron-react");
 const $ = React.createElement;
 
-
-
 class TextPlane extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render() {
@@ -26,7 +24,7 @@ class TextPlane extends React.Component {
   async componentDidUpdate() {
     if (!this.gl) {
       this.gl = this.refs.canvas.getContext("webgl2");
-      this.renderer = new Renderer(this.gl)
+      this.renderer = new Renderer(this.gl);
     }
 
     const {
@@ -39,7 +37,7 @@ class TextPlane extends React.Component {
 
     const computedLineHeight = Math.ceil(lineHeight * fontSize);
 
-    const ctx = this.ctx
+    const ctx = this.ctx;
 
     // Fill background
 
@@ -56,65 +54,207 @@ TextPlane.contextTypes = {
 
 module.exports = TextPlane;
 
-const shaders = require('./shaders')
-const TEXT_BLEND_ATTRIBUTE_NAMES = [
-  "unitQuadCorner",
-  "targetOrigin",
-  "targetSize",
-  "textColorRGBA",
-  "atlasOrigin",
-  "atlasSize"
-]
-const UNIT_QUAD_CORNERS = new Float32Array([
-  1, 1,
-  1, 0,
-  0, 0,
-  0, 1
-])
+const shaders = require("./shaders");
+const UNIT_QUAD_VERTICES = new Float32Array([1, 1, 1, 0, 0, 0, 0, 1]);
+const UNIT_QUAD_ELEMENT_INDICES = new Float32Array([0, 1, 3, 1, 2, 3]);
+const MAX_GLYPH_INSTANCES = 1 << 16;
+const GLYPH_INSTANCE_SIZE_IN_BYTES = 12 * Float32Array.BYTES_PER_ELEMENT;
 
 class Renderer {
-  constructor (gl) {
+  constructor(gl) {
     this.gl = gl;
+    this.atlas = new Atlas(gl);
 
-    const textBlendVertexShader = this.createShader(shaders.textBlendVertex, this.gl.VERTEX_SHADER)
-    const textBlendPass1FragmentShader = this.createShader(shaders.textBlendPass1Fragment, this.gl.FRAGMENT_SHADER)
-    const textBlendPass2FragmentShader = this.createShader(shaders.textBlendPass2Fragment, this.gl.FRAGMENT_SHADER)
+    const textBlendVertexShader = this.createShader(
+      shaders.textBlendVertex,
+      this.gl.VERTEX_SHADER
+    );
+    const textBlendPass1FragmentShader = this.createShader(
+      shaders.textBlendPass1Fragment,
+      this.gl.FRAGMENT_SHADER
+    );
+    const textBlendPass2FragmentShader = this.createShader(
+      shaders.textBlendPass2Fragment,
+      this.gl.FRAGMENT_SHADER
+    );
 
-    this.textBlendPass1Program = this.createProgram(textBlendVertexShader, textBlendPass1FragmentShader);
-    this.textBlendPass2Program = this.createProgram(textBlendVertexShader, textBlendPass2FragmentShader);
-    // Both programs declare the same attributes in the same order, so we can
-    // use the same attribute locations for both.
-    this.textBlendAttributeLocations = this.getAttributeLocations(this.textBlendPass1Program, TEXT_BLEND_ATTRIBUTE_NAMES)
+    this.textBlendPass1Program = this.createProgram(
+      textBlendVertexShader,
+      textBlendPass1FragmentShader
+    );
+    this.textBlendPass2Program = this.createProgram(
+      textBlendVertexShader,
+      textBlendPass2FragmentShader
+    );
 
-    this.unitQuadCornersBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitQuadCornersBuffer);
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, UNIT_QUAD_CORNERS, gl.STATIC_DRAW);
-    this.gl.vertexAttribPointer(this.textBlendAttributeLocations.unitQuadCorner, 2, this.gl.FLOAT, false, 2 * UNIT_QUAD_CORNERS.BYTES_PER_ELEMENT, 0);
-    this.gl.enableVertexAttribArray(this.textBlendAttributeLocations.unitQuadCorner)
+    this.textBlendVAO = this.gl.createVertexArray();
+    this.gl.bindVertexArray(this.textBlendVAO);
+
+    this.unitQuadVerticesBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.unitQuadVerticesBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      UNIT_QUAD_VERTICES,
+      gl.STATIC_DRAW
+    );
+
+    this.unitQuadElementIndicesBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(
+      this.gl.ELEMENT_ARRAY_BUFFER,
+      this.unitQuadElementIndicesBuffer
+    );
+    this.gl.bufferData(
+      this.gl.ELEMENT_ARRAY_BUFFER,
+      UNIT_QUAD_ELEMENT_INDICES,
+      gl.STATIC_DRAW
+    );
+
+    this.gl.enableVertexAttribArray(shaders.attributes.unitQuadVertex);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.unitQuadVertex,
+      2,
+      this.gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    this.glyphInstances = new Float32Array(MAX_GLYPH_INSTANCES);
+    this.glyphInstancesBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.glyphInstancesBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      this.glyphInstances,
+      this.gl.STREAM_DRAW
+    );
+
+    this.gl.enableVertexAttribArray(shaders.attributes.targetOrigin);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.targetOrigin,
+      2,
+      this.gl.FLOAT,
+      false,
+      GLYPH_INSTANCE_SIZE_IN_BYTES,
+      0
+    );
+    this.gl.vertexAttribDivisor(shaders.attributes.targetOrigin, 1);
+
+    this.gl.enableVertexAttribArray(shaders.attributes.targetSize);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.targetSize,
+      2,
+      this.gl.FLOAT,
+      false,
+      GLYPH_INSTANCE_SIZE_IN_BYTES,
+      2 * Float32Array.BYTES_PER_ELEMENT
+    );
+    this.gl.vertexAttribDivisor(shaders.attributes.targetSize, 1);
+
+    this.gl.enableVertexAttribArray(shaders.attributes.textColorRGBA);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.textColorRGBA,
+      4,
+      this.gl.FLOAT,
+      false,
+      GLYPH_INSTANCE_SIZE_IN_BYTES,
+      4 * Float32Array.BYTES_PER_ELEMENT
+    );
+    this.gl.vertexAttribDivisor(shaders.attributes.textColorRGBA, 1);
+
+    this.gl.enableVertexAttribArray(shaders.attributes.atlasOrigin);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.atlasOrigin,
+      2,
+      this.gl.FLOAT,
+      false,
+      GLYPH_INSTANCE_SIZE_IN_BYTES,
+      8 * Float32Array.BYTES_PER_ELEMENT
+    );
+    this.gl.vertexAttribDivisor(shaders.attributes.atlasOrigin, 1);
+
+    this.gl.enableVertexAttribArray(shaders.attributes.atlasSize);
+    this.gl.vertexAttribPointer(
+      shaders.attributes.atlasSize,
+      2,
+      this.gl.FLOAT,
+      false,
+      GLYPH_INSTANCE_SIZE_IN_BYTES,
+      10 * Float32Array.BYTES_PER_ELEMENT
+    );
+    this.gl.vertexAttribDivisor(shaders.attributes.atlasSize, 1);
   }
 
-  createProgram (vertexShader, fragmentShader) {
+  createProgram(vertexShader, fragmentShader) {
     const program = this.gl.createProgram();
     this.gl.attachShader(program, vertexShader);
     this.gl.attachShader(program, fragmentShader);
     this.gl.linkProgram(program);
+    if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+      var info = this.gl.getProgramInfoLog(program);
+      throw "Could not compile WebGL program: \n\n" + info;
+    }
     this.gl.useProgram(program);
-    return program
+    return program;
   }
 
-  createShader (source, type) {
+  createShader(source, type) {
     const shader = this.gl.createShader(type);
     this.gl.shaderSource(shader, source);
     this.gl.compileShader(shader);
-    return shader
+
+    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      var info = this.gl.getShaderInfoLog(shader);
+      throw "Could not compile WebGL program: \n\n" + info;
+    }
+
+    return shader;
   }
 
-  getAttributeLocations (program, attributeNames) {
-    const locations = {}
+  getAttributeLocations(program, attributeNames) {
+    const locations = {};
     for (let i = 0; i < attributeNames.length; i++) {
       const name = attributeNames[i];
       locations[name] = this.gl.getAttribLocation(program, name);
     }
     return locations;
+  }
+}
+
+class Atlas {
+  constructor(gl) {
+    const size = 512;
+
+    this.texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, this.texture);
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.SRGB8,
+      size,
+      size,
+      0,
+      gl.RGB,
+      gl.UNSIGNED_BYTE,
+      new ImageData(size, size)
+    );
+
+    this.gl = gl;
+    this.glyphCanvas = document.createElement("canvas");
+    this.glyphCanvas.width = size;
+    this.glyphCanvas.height = size;
+    this.glyphCtx = this.glyphCanvas.getContext("2d", { alpha: false });
+    this.uvScale = 1 / size;
+  }
+
+  getGlyph(text) {
+    this.glyphCtx.fillStyle = "white";
+    this.glyphCtx.fillRect(
+      0,
+      0,
+      this.glyphCanvas.width,
+      this.glyphCanvas.height
+    );
+    this.glyphCtx.fillStyle = "black";
+    this.glyphCtx.fillText(text, 0, 0);
   }
 }
