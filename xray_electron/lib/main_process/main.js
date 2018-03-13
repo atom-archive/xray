490dd8183332b8d178fb20779880d42d6818e24e
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-03-13 14:47:18 -0700

    Streamline electron main process
    Co-authored by: Nathan Sobo <nathan@github.com>

--- xray_electron/lib/main_process/main.js
+++ xray_electron/lib/main_process/main.js
@@ -4,74 +4,68 @@ const path = require('path');
 const url = require('url');
 const net = require('net');
 
+const SERVER_PATH = path.join(__dirname, '..', '..', '..', 'target', 'debug', 'xray_server');
+
 const SOCKET_PATH = process.env.XRAY_SOCKET_PATH;
 if (!SOCKET_PATH) {
   console.error('Missing XRAY_SOCKET_PATH environment variable');
   process.exit(1);
 }
 
-const SERVER_PATH = path.join(__dirname, '..', '..', '..', 'target', 'debug', 'xray_server');
-
-const socketPromise = new Promise((resolve, reject) => {
-  let serverProcess = spawn(SERVER_PATH, [], {stdio: ['ignore', 'pipe', 'inherit']});
-  app.on('before-quit', () => serverProcess.kill());
-
-  let serverStdout = '';
-  serverProcess.stdout.on('data', data => {
-    serverStdout += data.toString('utf8');
-    if (serverStdout.includes('Listening\n')) {
-      const socket = net.connect(SOCKET_PATH, () => resolve(socket));
-      socket.on('error', reject);
-    }
-  });
-
-  serverProcess.on('error', reject);
-
-  serverProcess.on('exit', () => app.quit());
-});
-
-const readyPromise = new Promise(resolve =>
-  app.on('ready', resolve)
-);
-
-Promise.all([socketPromise, readyPromise]).then(([socket]) =>
-  (new XrayApplication(SOCKET_PATH, socket)).start()
-);
+const INITIAL_MESSAGE = process.env.XRAY_INITIAL_MESSAGE;
 
 class XrayApplication {
-  constructor (socketPath, serverSocket) {
-    this.socketPath;
-    this.serverSocket = serverSocket;
+  constructor (serverPath, socketPath) {
+    this.serverPath = serverPath;
+    this.socketPath = socketPath;
     this.messageChunks = [];
     this.windowsByWorkspaceId = new Map();
+    this.readyPromise = new Promise(resolve => app.on('ready', resolve));
   }
 
-  start () {
-    this.serverSocket.on('data', this.handleChunk);
-    this.sendMessage({type: 'StartApplication'});
+  async start (initialMessage) {
+    const serverProcess = spawn(this.serverPath, [], {stdio: ['ignore', 'pipe', 'inherit']});
+    app.on('before-quit', () => serverProcess.kill());
 
-    const initialMessage = process.env.XRAY_INITIAL_MESSAGE;
+    let serverStdout = '';
+    this.serverSocket = await new Promise((resolve, reject) => {
+      serverProcess.stdout.on('data', data => {
+        serverStdout += data.toString('utf8');
+        if (serverStdout.includes('Listening\n')) {
+          const socket = net.connect(SOCKET_PATH, () => resolve(socket));
+          socket.on('error', reject);
+        }
+      });
+    })
+
+    serverProcess.on('error', console.error);
+    serverProcess.on('exit', () => app.quit());
+
+    this.serverSocket.on('data', this._handleChunk);
+    this._sendMessage({type: 'StartApplication'});
+
     if (initialMessage) {
-      this.sendMessage(JSON.parse(initialMessage));
+      this._sendMessage(JSON.parse(initialMessage));
     }
   }
 
-  sendMessage (message) {
+  _sendMessage (message) {
     this.serverSocket.write(JSON.stringify(message));
     this.serverSocket.write('\n');
   }
 
-  handleMessage (message) {
+  async _handleMessage (message) {
+    await this.readyPromise;
     switch (message.type) {
       case 'OpenWindow': {
         const workspaceId = message.workspaceId;
-        this.createWindow(workspaceId);
+        this._createWindow(workspaceId);
         break;
       }
     }
   }
 
-  handleChunk (chunk) {
+  _handleChunk (chunk) {
     const newlineIndex = chunk.indexOf('\n');
     if (newlineIndex !== -1) {
       this.messageChunks.push(chunk.slice(0, newlineIndex));
@@ -85,13 +79,14 @@ class XrayApplication {
     }
   }
 
-  createWindow (workspaceId) {
+  _createWindow (workspaceId) {
     const window = new BrowserWindow({width: 800, height: 600});
     window.loadURL(url.format({
       pathname: path.join(__dirname, `../../index.html?workspaceId=${workspaceId}&socketPath=${this.socketPath}`),
       protocol: 'file:',
       slashes: true
     }));
+    this.windowsByWorkspaceId.set(workspaceId, window);
     window.on('closed', () => this.windowsByWorkspaceId.delete(workspaceId));
   }
 }
@@ -103,3 +98,6 @@ app.on('window-all-closed', function () {
     app.quit();
   }
 });
+
+const application = new XrayApplication(SERVER_PATH, SOCKET_PATH);
+application.start(INITIAL_MESSAGE);
