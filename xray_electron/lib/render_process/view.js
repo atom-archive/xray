0697968d0af7b8f9d00b3f9dfbc1b55c4f049f66
Antonio Scandurra <as-cii@github.com>
2018-03-19 11:41:56 +0100

    Introduce View React component
    This React component will interact with the view registry to render a
    the component associated with the supplied view id. When the
    corresponding props change, this component will re-render its subtree.
    
    `View` also exposes a `dispatch` function to its underlying component.
    This is a convenient wrapper that automatically routes actions to the
    appropriate view on the server.

---
+++ xray_electron/lib/render_process/view.js
@@ -0,0 +1,54 @@
+const propTypes = require("prop-types");
+const React = require("react");
+const $ = React.createElement;
+const ViewRegistry = require("./view_registry");
+
+class View extends React.Component {
+  constructor(props, context) {
+    super(props);
+    this.state = {
+      version: 0,
+      viewId: props.id,
+      disposePropsWatch: context.viewRegistry.watchProps(props.id, () => {
+        this.setState({ version: this.state.version++ });
+      })
+    };
+  }
+
+  componentWillReceiveProps(props, context) {
+    this.handlePropsChange(props, context);
+  }
+
+  componentWillUnmount() {
+    if (this.state.disposePropsWatch) this.state.disposePropsWatch();
+  }
+
+  render() {
+    const { viewRegistry } = this.context;
+    const { id } = this.props;
+    const component = viewRegistry.getComponent(id);
+    const props = viewRegistry.getProps(id);
+    const dispatch = action => viewRegistry.dispatchAction(id, action);
+    return $(component, Object.assign({ dispatch, key: id }, props));
+  }
+
+  handlePropsChange(props, context) {
+    const { viewRegistry } = context;
+
+    if (this.state.viewId !== props.id) {
+      this.state.disposePropsWatch();
+      this.setState({
+        viewId: props.id,
+        disposePropsWatch: viewRegistry.watchProps(props.id, () => {
+          this.setState({ version: this.state.version + 1 });
+        })
+      });
+    }
+  }
+}
+
+View.contextTypes = {
+  viewRegistry: propTypes.instanceOf(ViewRegistry)
+};
+
+module.exports = View;
