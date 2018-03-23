75f044fbc37085e44afabe6c969953073f6772b8
Max Brunsfeld <maxbrunsfeld@gmail.com>
2018-03-23 12:35:08 -0700

    wip

--- xray_core/src/fs.rs
+++ xray_core/src/fs.rs
@@ -1,17 +1,36 @@
 use futures::Stream;
 use parking_lot::RwLock;
-use std::collections::BTreeMap;
-use std::ffi::{OsStr, OsString};
-use std::path::Path;
+use std::collections::BinaryHeap;
+use std::ffi::OsString;
+use std::path::{Path, PathBuf};
 use std::result;
 use std::sync::Arc;
+use notify_cell::{NotifyCell, NotifyCellObserver};
+use futures::{Future, Poll};
 
 pub type Result<T> = result::Result<T, ()>;
+type Entries = Vec<(OsString, Entry)>;
 
 pub trait Tree {
     fn path(&self) -> &Path;
     fn root(&self) -> &Entry;
     fn updates(&self) -> Box<Stream<Item = (), Error = ()>>;
+
+    fn search_results() -> NotifyCellObserver<Vec<SearchResult>>;
+
+    fn search(&self, query: &str, max_results: usize) -> Result<Search> {
+        match self.root() {
+            &Entry::Dir(ref inner) => {
+                Ok(Search {
+                    query: query.to_owned(),
+                    max_results,
+                    results: BinaryHeap::new(),
+                    stack: vec![(inner.read().entries.clone(), 0)]
+                })
+            },
+            _ => Err(())
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -22,13 +41,37 @@ pub enum Entry {
 
 #[derive(Clone, Debug)]
 pub struct DirInner {
-    entries: Arc<Vec<(OsString, Entry)>>,
+    entries: Arc<Entries>,
     is_symlink: bool,
 }
 
 #[derive(Clone, Debug)]
 pub struct FileInner {
     is_symlink: bool,
+}
+
+pub struct Search {
+    query: String,
+    max_results: usize,
+    results: BinaryHeap<SearchResult>,
+    stack: Vec<(Arc<Entries>, usize)>
+}
+
+pub struct SearchResult {
+    score: usize,
+    path: PathBuf,
+    matches: Vec<usize>
+}
+
+struct StackEntry {
+    entries: Arc<Entries>,
+    entries_index: usize,
+    match_variants: Vec<MatchVariant>
+}
+
+struct MatchVariant {
+    query_index: u16,
+    score: usize,
 }
 
 impl Entry {
@@ -71,6 +114,65 @@ impl Entry {
         }
     }
 }
+
+impl Search {
+    pub fn results(&self) -> NotifyCellObserver<Vec<SearchResult>> {
+        self.updates.observe()
+    }
+}
+
+// bob/axelllllllasdjfklasdfadsfdasf/a
+
+// root: bob
+// query: bxx
+
+// (bob, 0, [{query_index: 0, score: 0}, {query_index: 1, score: 5}, {query_index: 1, score: 3}])
+// (axel, 0, [{query_index: 0, score: 0}, {query_index: 1, score: 5}, {query_index: 1, score: 3}, {query_index: 2, score: 6}])
+
+
+impl Future for Search {
+    type Item = ();
+    type Error = ();
+
+    fn poll(&mut self) -> Poll<(), ()> {
+        loop {
+            let cur_dir = self.stack.last();
+            if cur_dir.entries_index == cur_dir.entries.len() {
+                self.parent_path.pop();
+                if let Some(last) = self.stack.pop() {
+                    self.match_variants.truncate(last.prev_variants_len);
+                } else {
+                    Async::Ready(())
+                }
+            } else {
+                let child = cur_dir.entries[index];
+                match child {
+                    Entry::Dir(ref inner) => {
+                        self.parent_path.push(name);
+                        let prev_variants_len = self.match_variants.len();
+                        compute_match_variants(&mut self.match_variants, query, inner.name);
+                        self.stack.push(StackEntry {
+                            entries: inner.entries.clone(),
+                            entries_index: 0,
+                            prev_variants_len
+                        })
+                    },
+                    Entry::File(ref inner) => {
+                        // Fuzzy match on file name.
+
+                        index += 1;
+                    }
+                }
+            }
+        }
+
+
+        // scan tree...
+        // every now and then call notifyCell.set(latest_result)
+        // Async::NotReady()
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
