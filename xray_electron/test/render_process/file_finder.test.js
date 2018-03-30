cf374cd091683a83269d2d057b9b5ad49b7a19bf
Antonio Scandurra <as-cii@github.com>
2018-03-30 18:31:20 +0200

    Fix FileFinder JS tests
    Co-authored-by: Nathan Sobo <nathan@github.com>

--- xray_electron/test/render_process/file_finder.test.js
+++ xray_electron/test/render_process/file_finder.test.js
@@ -15,8 +15,8 @@ suite("FileFinderView", () => {
     await setProps(fileFinder, {
       query: 'ce',
       results: [
-        {path: 'succeed', score: 3, positions: [3, 4]},
-        {path: 'abcdef', score: 2, positions: [2, 4]},
+        {relative_path: 'succeed', score: 3, positions: [3, 4]},
+        {relative_path: 'abcdef', score: 2, positions: [2, 4]},
       ]
     });
 
