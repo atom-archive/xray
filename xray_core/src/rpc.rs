1ff1d3ac4a288c06bec92dd8f725ad3f6180d78c
Antonio Scandurra <as-cii@github.com>
2018-04-06 11:27:24 +0200

    WIP
    Co-authored-by: Nathan Sobo <nathan@github.com>

--- xray_core/src/rpc.rs
+++ xray_core/src/rpc.rs
@@ -1,10 +1,11 @@
 use bincode::{deserialize, serialize};
-use bytes::Bytes;
+use futures::stream::FuturesUnordered;
 use futures::task::{self, Task};
-use futures::{Async, Future, Poll, Stream};
+use futures::{future, Async, Future, Poll, Stream};
 use serde::{Deserialize, Serialize};
 use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
+use std::io;
 use std::mem;
 use std::rc::Rc;
 
@@ -19,10 +20,7 @@ pub trait Service {
     type Error: 'static + Serialize;
 
     fn state(&self, connection: &mut ConnectionToClient) -> Self::State;
-    fn poll_updates(
-        &mut self,
-        connection: &mut ConnectionToClient,
-    ) -> Async<Option<Vec<Self::Update>>>;
+    fn poll_update(&mut self, connection: &mut ConnectionToClient) -> Async<Option<Self::Update>>;
     fn request(
         &mut self,
         _request: Self::Request,
@@ -32,56 +30,68 @@ pub trait Service {
     }
 }
 
-trait ErasedService {
+trait RawBytesService {
     fn state(&self, connection: &mut ConnectionToClient) -> Vec<u8>;
-    fn poll_updates(&mut self, connection: &mut ConnectionToClient) -> Async<Option<Vec<Vec<u8>>>>;
+    fn poll_update(&mut self, connection: &mut ConnectionToClient) -> Async<Option<Vec<u8>>>;
     fn request(
         &mut self,
-        request: Bytes,
+        request: Vec<u8>,
         connection: &mut ConnectionToClient,
     ) -> Option<Box<Future<Item = Vec<u8>, Error = Vec<u8>>>>;
 }
 
-trait ErasedResponseFuture {
-    fn poll(&mut self) -> Async<Vec<u8>>;
+#[derive(Serialize, Deserialize)]
+enum MessageToClient {
+    Update {
+        insertions: HashMap<ServiceId, Vec<u8>>,
+        updates: HashMap<ServiceId, Vec<Vec<u8>>>,
+        removals: HashSet<ServiceId>,
+        responses: HashMap<ServiceId, Vec<(RequestId, Response)>>,
+    },
+    Err(String),
 }
 
 #[derive(Serialize, Deserialize)]
-struct MessageToClient {
-    insertions: HashMap<ServiceId, Vec<u8>>,
-    updates: HashMap<ServiceId, Vec<Vec<u8>>>,
-    removals: HashSet<ServiceId>,
-    responses: Vec<(ServiceId, RequestId, Result<Vec<u8>, Vec<u8>>)>,
+enum Response {
+    Ok(Vec<u8>),
+    Err(Vec<u8>),
+    RpcErr(RpcError),
+}
+
+#[derive(Serialize, Deserialize)]
+enum RpcError {
+    ServiceNotFound,
 }
 
 #[derive(Serialize, Deserialize)]
 enum MessageToServer {
     Request {
-        service: ServiceId,
-        id: RequestId,
+        service_id: ServiceId,
+        request_id: RequestId,
         payload: Vec<u8>,
     },
 }
 
 pub struct ConnectionToClient {
     next_id: ServiceId,
-    services: HashMap<ServiceId, Rc<RefCell<ErasedService>>>,
+    services: HashMap<ServiceId, Rc<RefCell<RawBytesService>>>,
     inserted: HashSet<ServiceId>,
     removed: HashSet<ServiceId>,
-    requests: Box<Stream<Item = Vec<u8>, Error = ()>>,
+    incoming: Box<Stream<Item = Vec<u8>, Error = io::Error>>,
+    pending_responses: FuturesUnordered<Box<Future<Item = ResponseEnvelope, Error = ()>>>,
     pending_task: Option<Task>,
 }
 
-struct ResponseFuture<T: Future> {
-    service: ServiceId,
+struct ResponseEnvelope {
+    service_id: ServiceId,
     request_id: RequestId,
-    response: T,
+    response: Response,
 }
 
 impl ConnectionToClient {
-    pub fn new<S, T>(requests: S, bootstrap: T) -> Self
+    pub fn new<S, T>(incoming: S, bootstrap: T) -> Self
     where
-        S: 'static + Stream<Item = Vec<u8>, Error = ()>,
+        S: 'static + Stream<Item = Vec<u8>, Error = io::Error>,
         T: 'static + Service,
     {
         let mut connection = Self {
@@ -89,7 +99,8 @@ impl ConnectionToClient {
             services: HashMap::new(),
             inserted: HashSet::new(),
             removed: HashSet::new(),
-            requests: Box::new(requests),
+            incoming: Box::new(incoming),
+            pending_responses: FuturesUnordered::new(),
             pending_task: None,
         };
         connection.add_service(bootstrap);
@@ -104,24 +115,50 @@ impl ConnectionToClient {
         id
     }
 
-    fn poll_requests(&mut self) -> Poll<Option<()>, ()> {
-        match self.requests.poll() {
-            Ok(Async::Ready(Some(request))) => match deserialize(&request).unwrap() {
-                MessageToServer::Request {
-                    service,
-                    id,
-                    payload,
-                } => {
-                    if let Some(service) = self.services.get(&service) {
-                        service.borrow_mut().request(id, payload)
+    fn poll_incoming(&mut self) -> Result<bool, io::Error> {
+        loop {
+            match self.incoming.poll() {
+                Ok(Async::Ready(Some(request))) => match deserialize(&request).unwrap() {
+                    MessageToServer::Request {
+                        request_id,
+                        service_id,
+                        payload,
+                    } => {
+                        if let Some(service) = self.services.get(&service_id).cloned() {
+                            if let Some(response) = service.borrow_mut().request(payload, self) {
+                                self.pending_responses
+                                    .push(Box::new(response.then(move |response| {
+                                        Ok(ResponseEnvelope {
+                                            request_id,
+                                            service_id,
+                                            response: match response {
+                                                Ok(payload) => Response::Ok(payload),
+                                                Err(payload) => Response::Err(payload),
+                                            },
+                                        })
+                                    })));
+                            }
+                        } else {
+                            self.pending_responses
+                                .push(Box::new(future::ok(ResponseEnvelope {
+                                    request_id,
+                                    service_id,
+                                    response: Response::RpcErr(RpcError::ServiceNotFound),
+                                })));
+                        }
                     }
+                },
+                Ok(Async::Ready(None)) => return Ok(false),
+                Ok(Async::NotReady) => return Ok(true),
+                Err(error) => {
+                    eprintln!("Error polling incoming connection: {}", error);
+                    return Err(error);
                 }
-            },
-            result @ _ => result,
+            }
         }
     }
 
-    fn poll_updates(&mut self) -> Poll<Option<MessageToClient>, ()> {
+    fn poll_outgoing(&mut self) -> Poll<Option<Vec<u8>>, ()> {
         let mut insertions = HashMap::new();
         let mut inserted = HashSet::new();
         mem::swap(&mut inserted, &mut self.inserted);
@@ -135,28 +172,44 @@ impl ConnectionToClient {
         for id in service_ids {
             let service = self.services.get(&id).unwrap().clone();
             let mut service_borrow = service.borrow_mut();
-            match service_borrow.poll_updates(self) {
-                Async::Ready(Some(service_updates)) => {
-                    if !inserted.contains(&id) {
-                        updates.insert(id, service_updates);
+            loop {
+                match service_borrow.poll_update(self) {
+                    Async::Ready(Some(update)) => {
+                        if !inserted.contains(&id) {
+                            updates.entry(id).or_insert(Vec::new()).push(update);
+                        }
                     }
+                    Async::Ready(None) => unimplemented!("Terminate the service"),
+                    Async::NotReady => break,
                 }
-                Async::Ready(None) => {
-                    // TODO: Terminate service
-                }
-                Async::NotReady => {}
             }
         }
 
         let mut removals = HashSet::new();
         mem::swap(&mut removals, &mut self.removed);
 
-        if insertions.len() > 0 || updates.len() > 0 || removals.len() > 0 {
-            Ok(Async::Ready(Some(MessageToClient::Update {
+        let mut responses = HashMap::new();
+        loop {
+            match self.pending_responses.poll() {
+                Ok(Async::Ready(Some(envelope))) => {
+                    responses
+                        .entry(envelope.service_id)
+                        .or_insert(Vec::new())
+                        .push((envelope.request_id, envelope.response));
+                }
+                Ok(Async::Ready(None)) | Ok(Async::NotReady) => break,
+                Err(_) => unreachable!(),
+            }
+        }
+
+        if insertions.len() > 0 || updates.len() > 0 || removals.len() > 0 || responses.len() > 0 {
+            let message = serialize(&MessageToClient::Update {
                 insertions,
                 updates,
                 removals,
-            })))
+                responses,
+            }).unwrap();
+            Ok(Async::Ready(Some(message)))
         } else {
             self.pending_task = Some(task::current());
             Ok(Async::NotReady)
@@ -169,26 +222,21 @@ impl Stream for ConnectionToClient {
     type Error = ();
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        
-    }
-}
-
-impl<T, I, E> ErasedResponseFuture for ResponseFuture<T>
-where
-    T: Future<Item = I, Error = E>,
-    I: Serialize,
-    E: Serialize,
-{
-    fn poll(&mut self) -> Async<Vec<u8>> {
-        match self.response.poll() {
-            Ok(Async::Ready(result)) => Async::Ready(serialize(&result).unwrap()),
-            Ok(Async::NotReady) => Async::NotReady,
-            Err(error) => Async::Ready,
+        match self.poll_incoming() {
+            Ok(true) => {}
+            Ok(false) => return Ok(Async::Ready(None)),
+            Err(error) => {
+                let description = format!("{}", error);
+                let message = serialize(&MessageToClient::Err(description)).unwrap();
+                return Ok(Async::Ready(Some(message)));
+            }
         }
+
+        self.poll_outgoing()
     }
 }
 
-impl<T> ErasedService for T
+impl<T> RawBytesService for T
 where
     T: Service,
 {
@@ -196,20 +244,14 @@ where
         serialize(&T::state(self, connection)).unwrap()
     }
 
-    fn poll_updates(&mut self, connection: &mut ConnectionToClient) -> Async<Option<Vec<Vec<u8>>>> {
-        T::poll_updates(self, connection).map(|option| {
-            option.map(|updates| {
-                updates
-                    .into_iter()
-                    .map(|update| serialize(&update).unwrap())
-                    .collect()
-            })
-        })
+    fn poll_update(&mut self, connection: &mut ConnectionToClient) -> Async<Option<Vec<u8>>> {
+        T::poll_update(self, connection)
+            .map(|option| option.map(|update| serialize(&update).unwrap()))
     }
 
     fn request(
         &mut self,
-        request: Bytes,
+        request: Vec<u8>,
         connection: &mut ConnectionToClient,
     ) -> Option<Box<Future<Item = Vec<u8>, Error = Vec<u8>>>> {
         T::request(self, deserialize(&request).unwrap(), connection).map(|future| {
