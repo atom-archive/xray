86d7d20d594a7298fe3fceadd9826767d9e85a9d
Antonio Scandurra <as-cii@github.com>
2018-04-26 14:01:14 +0200

    Style file finder so that it looks slightly better

--- xray_ui/lib/file_finder.js
+++ xray_ui/lib/file_finder.js
@@ -4,34 +4,57 @@ const { styled } = require("styletron-react");
 const $ = React.createElement;
 
 const Root = styled("div", {
-  boxShadow: '0 0 8px black',
-  backgroundColor: 'white',
-  width: 500 + 'px',
-  padding: "10px"
+  boxShadow: "0 6px 12px -2px rgba(0, 0, 0, 0.4)",
+  backgroundColor: "#f2f2f2",
+  borderRadius: "6px",
+  width: 500 + "px",
+  padding: "10px",
+  marginTop: "20px"
 });
 
 const QueryInput = styled("input", {
   width: "100%",
-  boxSizing: "border-box"
+  boxSizing: "border-box",
+  padding: "5px",
+  fontSize: "10pt",
+  outline: "none",
+  border: "1px solid #556de8",
+  boxShadow: "0 0 0 1px #556de8",
+  backgroundColor: "#ebeeff",
+  borderRadius: "3px",
+  color: "#232324"
 });
 
 const SearchResultList = styled("ol", {
-  listStyleType: 'none',
-  height: '200px',
-  overflow: 'auto',
-  padding: 0,
+  listStyleType: "none",
+  height: "200px",
+  overflow: "auto",
+  padding: 0
 });
 
 const SearchResultListItem = styled("li", {
-  listStyleType: 'none',
-  marginTop: '10px'
+  listStyleType: "none",
+  padding: "0.75em 1em",
+  lineHeight: "2em",
+  fontSize: "10pt",
+  fontFamily: "sans-serif",
+  borderBottom: "1px solid #dbdbdc"
+});
+
+const SearchResultMatchedQuery = styled("b", {
+  color: "#304ee2",
+  fontWeight: "bold"
 });
 
 class SelectedSearchResultListItem extends React.Component {
   render() {
-    return $(styled(SearchResultListItem, {
-      backgroundColor: 'blue'
-    }), {}, ...this.props.children);
+    return $(
+      styled(SearchResultListItem, {
+        backgroundColor: "#dbdbdc"
+      }),
+      {},
+      ...this.props.children
+    );
   }
 
   componentDidMount() {
@@ -39,12 +62,12 @@ class SelectedSearchResultListItem extends React.Component {
   }
 
   componentDidUpdate() {
-    this.scrollIntoViewIfNeeded()
+    this.scrollIntoViewIfNeeded();
   }
 
   scrollIntoViewIfNeeded() {
     const domNode = ReactDOM.findDOMNode(this);
-    if (domNode) domNode.scrollIntoViewIfNeeded()
+    if (domNode) domNode.scrollIntoViewIfNeeded();
   }
 }
 
@@ -57,34 +80,34 @@ module.exports = class FileFinder extends React.Component {
   }
 
   render() {
-    return $(Root, null,
+    return $(
+      Root,
+      null,
       $(QueryInput, {
-        $ref: (inputNode) => this.queryInput = inputNode,
+        $ref: inputNode => (this.queryInput = inputNode),
         value: this.props.query,
         onChange: this.didChangeQuery,
-        onKeyDown: this.didKeyDown,
+        onKeyDown: this.didKeyDown
       }),
-      $("label", {},
-        $("input", {
-          type: 'checkbox',
-          onChange: this.didChangeIncludeIgnored,
-          checked: this.props.includeIgnored,
-        }),
-        "Include Ignored Files"
-      ),
-      $(SearchResultList, {}, ...this.props.results.map((result, i) =>
-        this.renderSearchResult(result, i === this.props.selected_index)
-      ))
+      $(
+        SearchResultList,
+        {},
+        ...this.props.results.map((result, i) =>
+          this.renderSearchResult(result, i === this.props.selected_index)
+        )
+      )
     );
   }
 
-  renderSearchResult({positions, display_path}, isSelected) {
+  renderSearchResult({ positions, display_path }, isSelected) {
     let pathIndex = 0;
     let queryIndex = 0;
     const children = [];
     while (true) {
       if (pathIndex === positions[queryIndex]) {
-        children.push($('b', null, display_path[pathIndex]));
+        children.push(
+          $(SearchResultMatchedQuery, null, display_path[pathIndex])
+        );
         pathIndex++;
         queryIndex++;
       } else if (queryIndex < positions.length) {
@@ -97,7 +120,9 @@ module.exports = class FileFinder extends React.Component {
       }
     }
 
-    const item = isSelected ? SelectedSearchResultListItem : SearchResultListItem;
+    const item = isSelected
+      ? SelectedSearchResultListItem
+      : SearchResultListItem;
     return $(item, null, ...children);
   }
 
@@ -121,17 +146,17 @@ module.exports = class FileFinder extends React.Component {
 
   didKeyDown(event) {
     switch (event.key) {
-      case 'ArrowUp':
-        this.props.dispatch({type: 'SelectPrevious'});
+      case "ArrowUp":
+        this.props.dispatch({ type: "SelectPrevious" });
         break;
-      case 'ArrowDown':
-        this.props.dispatch({type: 'SelectNext'});
+      case "ArrowDown":
+        this.props.dispatch({ type: "SelectNext" });
         break;
-      case 'Enter':
-        this.props.dispatch({type: 'Confirm'});
+      case "Enter":
+        this.props.dispatch({ type: "Confirm" });
         break;
-      case 'Escape':
-        this.props.dispatch({type: 'Close'});
+      case "Escape":
+        this.props.dispatch({ type: "Close" });
         break;
     }
   }
