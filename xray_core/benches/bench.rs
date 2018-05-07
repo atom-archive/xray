2a0e92855f302cc27ef5e01c225038fae7a17f5b
Antonio Scandurra <as-cii@github.com>
2018-05-07 11:45:35 +0200

    Benchmark add_selection_above and add_selection_below

--- xray_core/benches/bench.rs
+++ xray_core/benches/bench.rs
@@ -5,23 +5,61 @@ extern crate criterion;
 use criterion::Criterion;
 use std::cell::RefCell;
 use std::rc::Rc;
-use xray_core::buffer::Buffer;
+use xray_core::buffer::{Buffer, Point};
 use xray_core::buffer_view::BufferView;
 
-fn bench_edit() {
-    let content = String::from("abcdefghijklmnopqrstuvwxyz");
-    let mut buffer = Buffer::new(0);
-    buffer.edit(0..0, content.as_str());
-    let mut editor = BufferView::new(Rc::new(RefCell::new(buffer)), 0, None);
-    for _ in 0..content.len() {
-        editor.select_right();
-        editor.edit("-");
-    }
+fn add_selection(c: &mut Criterion) {
+    c.bench_function("add_selection_below", |b| {
+        b.iter_with_setup(
+            || {
+                let mut buffer_view = create_buffer_view(100);
+                for i in 0..100 {
+                    buffer_view.add_selection(Point::new(i, 0), Point::new(i, 0));
+                }
+                buffer_view
+            },
+            |mut buffer_view| buffer_view.add_selection_below(),
+        )
+    });
+    c.bench_function("add_selection_above", |b| {
+        b.iter_with_setup(
+            || {
+                let mut buffer_view = create_buffer_view(100);
+                for i in 0..100 {
+                    buffer_view.add_selection(Point::new(i, 0), Point::new(i, 0));
+                }
+                buffer_view
+            },
+            |mut buffer_view| buffer_view.add_selection_above(),
+        )
+    });
 }
 
 fn edit(c: &mut Criterion) {
-    c.bench_function("edit", |b| b.iter(|| bench_edit()));
+    c.bench_function("edit", |b| {
+        b.iter_with_setup(
+            || create_buffer_view(10),
+            |mut buffer_view| {
+                for _ in 0..25 {
+                    buffer_view.select_right();
+                    buffer_view.edit("-");
+                }
+            },
+        )
+    });
 }
 
-criterion_group!(benches, edit);
+fn create_buffer_view(lines: usize) -> BufferView {
+    let mut buffer = Buffer::new(0);
+    for i in 0..lines {
+        let len = buffer.len();
+        buffer.edit(
+            len..len,
+            format!("Lorem ipsum dolor sit amet {}\n", i).as_str(),
+        );
+    }
+    BufferView::new(Rc::new(RefCell::new(buffer)), 0, None)
+}
+
+criterion_group!(benches, edit, add_selection);
 criterion_main!(benches);
