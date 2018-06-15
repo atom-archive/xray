7a657dda4a4662c0d8db0bc0b104d1dda31c7df8
Nathan Sobo <nathan@github.com>
2018-06-14 21:16:26 -0600

    Flesh out cursor
    The cursor is always parked on a file and knows that file's name,
    metadata, and depth. The cursor starts at the first subdirectory of the
    tree and advances through a depth-first traversal on calls to next. The
    cursor can advance off the end or be created for an empty tree, in which
    case its methods return None.

--- src/fs2.rs
+++ src/fs2.rs
@@ -4,13 +4,12 @@ use smallvec::SmallVec;
 use std::cmp::Ordering;
 use std::ffi::{OsStr, OsString};
 use std::ops::{Add, AddAssign};
-use std::path::PathBuf;
 use std::sync::Arc;
 
 struct Tree {
     root_name: OsString,
     items: btree::Tree<Item>,
-    file_ids_by_inode: btree::Tree<InodeToFileId>,
+    // file_ids_by_inode: btree::Tree<InodeToFileId>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq)]
@@ -18,6 +17,7 @@ enum Item {
     Metadata {
         file_id: id::Unique,
         is_dir: bool,
+        inode: u64,
     },
     DirEntry {
         file_id: id::Unique,
@@ -48,6 +48,7 @@ enum Key {
 
 struct Metadata {
     inode: u64,
+    is_dir: bool,
 }
 
 struct Builder {
@@ -57,24 +58,31 @@ struct Builder {
 }
 
 struct Cursor {
-    stack: Vec<id::Unique>,
-    tree_cursor: btree::Cursor<Item>,
+    stack: Vec<btree::Cursor<Item>>,
 }
 
-impl Tree {}
+impl Tree {
+    fn new(root_name: OsString) -> Self {
+        Self {
+            root_name,
+            items: btree::Tree::new(),
+            // file_ids_by_inode: btree::Tree::new(),
+        }
+    }
+}
 
 impl Item {
-    fn name(&self) -> &OsStr {
+    fn is_dir_metadata(&self) -> bool {
         match self {
-            Item::DirEntry { name, .. } => name,
-            Item::Metadata { .. } => panic!(),
+            Item::Metadata { is_dir, .. } => *is_dir,
+            _ => false
         }
     }
 
-    fn is_dir(&self) -> bool {
+    fn is_dir_entry(&self) -> bool {
         match self {
-            Item::DirEntry { is_dir, .. } => *is_dir,
-            Item::Metadata { .. } => panic!(),
+            Item::DirEntry { .. } => true,
+            _ => false
         }
     }
 }
@@ -135,41 +143,110 @@ impl<'a> Add<&'a Self> for Key {
     }
 }
 
-impl Builder {
-    fn push_dir<S>(&mut self, name: &OsStr, metadata: Metadata, db: &S) -> Result<(), S::ReadError>
-    where
-        S: NodeStore<Item>,
-    {
-        let item = self.cursor.item(db)?;
-        if item.is_dir() {
-            match name.cmp(item.name()) {
-                Ordering::Less => {}// Inserted,
-                Ordering::Equal => {}// Check metadata to see if they're the same?
-                Ordering::Greater => {}// `item` got removed
-            }
-        } else {
-            // Inserted
-        }
-    }
-}
+// impl Builder {
+//     fn push_dir<S>(&mut self, name: &OsStr, metadata: Metadata, db: &S) -> Result<(), S::ReadError>
+//     where
+//         S: NodeStore<Item>,
+//     {
+//         let item = self.cursor.item(db)?;
+//         if item.is_dir() {
+//             match name.cmp(item.name()) {
+//                 Ordering::Less => {}// Inserted,
+//                 Ordering::Equal => {}// Check metadata to see if they're the same?
+//                 Ordering::Greater => {}// `item` got removed
+//             }
+//         } else {
+//             // Inserted
+//         }
+//     }
+// }
 
 impl Cursor {
-    fn new<S>(root_name: &OsStr, tree: &Tree, db: &S) -> Result<Self, S::ReadError>
+    pub fn new<S>(tree: &Tree, db: &S) -> Result<Self, S::ReadError>
     where
         S: NodeStore<Item>,
     {
-        let mut tree_cursor = tree.items.cursor();
-        tree_cursor.seek(&Key::default(), SeekBias::Left, db)?;
-        Ok(Self {
-            tree_cursor,
-            stack: vec![],
-        })
+        let mut root_cursor = tree.items.cursor();
+        root_cursor.seek(&Key::default(), SeekBias::Left, db)?;
+        if let Some(item) = root_cursor.item(db)? {
+            let mut cursor = Self {
+                stack: vec![root_cursor],
+            };
+            cursor.follow_entry(db)?;
+            Ok(cursor)
+        } else {
+            Ok(Self { stack: vec![] })
+        }
     }
 
-    fn item<S>(&self, db: &S) -> Result<Item, S::ReadError>
-    where
-        S: NodeStore<Item>,
-    {
-        Ok(self.tree_cursor.item(db)?.unwrap())
+    pub fn metadata<S: NodeStore<Item>>(&self, db: &S) -> Result<Option<Metadata>, S::ReadError> {
+        if let Some(cursor) = self.stack.last() {
+            match cursor.item(db)?.unwrap() {
+                Item::Metadata { is_dir, inode, .. } => Ok(Some(Metadata { is_dir, inode })),
+                _ => unreachable!(),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn name<S: NodeStore<Item>>(&self, db: &S) -> Result<Option<Arc<OsString>>, S::ReadError> {
+        if self.stack.len() > 1 {
+            let cursor = &self.stack[self.stack.len() - 2];
+            match cursor.item(db)?.unwrap() {
+                Item::DirEntry { name, .. } => Ok(Some(name.clone())),
+                _ => unreachable!(),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn depth(&self) -> usize {
+        self.stack.len().saturating_sub(1)
+    }
+
+    pub fn next<S: NodeStore<Item>>(&mut self, db: &S) -> Result<bool, S::ReadError> {
+        while !self.stack.is_empty() {
+            let found_entry = {
+                let mut cursor = self.stack.last_mut().unwrap();
+                let cur_item = cursor.item(db)?.unwrap();
+                if cur_item.is_dir_entry() || cur_item.is_dir_metadata() {
+                    cursor.next(db)?;
+                    if let Some(next_item) = cursor.item(db)? {
+                        next_item.is_dir_entry()
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                }
+            };
+
+            if found_entry {
+                self.follow_entry(db)?;
+                return Ok(true);
+            } else {
+                self.stack.pop();
+            }
+        }
+
+        Ok(false)
+    }
+
+    fn follow_entry<S: NodeStore<Item>>(&mut self, db: &S) -> Result<(), S::ReadError> {
+        let mut child_cursor;
+        {
+            let entry_cursor = self.stack.last().unwrap();
+            match entry_cursor.item(db)?.unwrap() {
+                Item::DirEntry { child_id, .. } => {
+                    child_cursor = entry_cursor.clone();
+                    child_cursor.seek(&Key::Metadata { file_id: child_id }, SeekBias::Left, db)?;
+                }
+                _ => panic!(),
+            }
+        }
+        self.stack.push(child_cursor);
+        Ok(())
     }
 }
