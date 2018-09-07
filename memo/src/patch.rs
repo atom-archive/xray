17167050cf10a4b9e070b4067a89bbf460b19699
Antonio Scandurra <as-cii@github.com>
2018-09-07 19:53:50 +0200

    WIP
    Co-Authored-By: Nathan Sobo <nathan@github.com>

--- memo/src/patch.rs
+++ memo/src/patch.rs
@@ -1,16 +1,28 @@
+use btree::{self, SeekBias};
 use buffer::{Buffer, Text};
+use std::collections::HashMap;
 use std::collections::HashSet;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 use std::ops::Range;
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 use time;
 
 type FileId = time::Local;
 
-enum Operation {}
+pub const ROOT_FILE_ID: time::Local = time::Local::DEFAULT;
+
+enum Operation {
+    RegisterBasePath {
+        parent_id: FileId,
+        components: Vec<(OsString, FileId)>
+    }
+}
 
 struct Patch {
-    
+    parent_refs: btree::Tree<ParentRefValue>,
+    child_refs: btree::Tree<ChildRefValue>,
+    file_id_aliases: HashMap<FileId, FileId>,
 }
 
 struct Changes {
@@ -20,9 +32,39 @@ struct Changes {
     edited: HashSet<FileId>,
 }
 
+enum Error {
+    InvalidPath,
+}
+
 impl Patch {
-    fn file_id(&mut self, path: &Path) -> (FileId, Option<Operation>) {
-        unimplemented!()
+    fn file_id(&mut self, path: &Path) -> Result<(FileId, Option<Operation>), Error> {
+        let path = path.into();
+
+        let mut cursor = self.child_refs.cursor();
+        let mut parent_id = ROOT_FILE_ID;
+        let mut unregistered_ancestor = true;
+        let mut op_components = Vec::new();
+
+        for name in path.components() {
+            let name = Arc::new(OsString::from(name.as_os_str()));
+            if parent_exists && cursor.seek(&ChildRefId { parent_id, name }, SeekBias::Left) {
+                let child_ref = cursor.item().unwrap();
+                if child_ref.is_visible() {
+                    parent_id = child_ref.parent_ref_id;
+                } else {
+                    return Err(Error::InvalidPath);
+                }
+            } else {
+                parent_exists = false;
+
+                self.local_clock.tick();
+                let file_id = self.local_clock;
+                registrations.push({parent_id, name, })
+
+            }
+        }
+
+        Some(ref_id)
     }
 
     fn insert<T>(&mut self, parent_id: FileId, name: &OsStr, text: T) -> (FileId, Operation)
