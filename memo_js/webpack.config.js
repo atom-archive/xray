331121cb2483a7b567506337e99a20fabda54fdb
Antonio Scandurra <as-cii@github.com>
2018-10-15 17:48:40 -0600

    Convert tests to TypeScript without breaking dist output
    Co-Authored-By: Nathan Sobo <nathan@github.com>

--- memo_js/webpack.config.js
+++ memo_js/webpack.config.js
@@ -1,7 +1,6 @@
 var path = require("path");
 
-module.exports = {
-  entry: "./src/index.ts",
+const baseConfig = {
   target: "node",
   module: {
     rules: [
@@ -14,11 +13,37 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".js", ".wasm"]
-  },
+  }
+};
+
+const libConfig = {
+  ...baseConfig,
+  entry: "./src/index.ts",
+  mode: "production",
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.node.js",
     library: "memo",
-    libraryTarget: "umd"
+    libraryTarget: "commonjs"
+  }
+};
+
+const testConfig = {
+  ...baseConfig,
+  entry: "./test/tests.ts",
+  mode: "development",
+  devtool: "cheap-eval-source-map",
+  output: {
+    path: path.resolve(__dirname, "test", "dist"),
+    filename: "tests.js"
+  }
+};
+
+module.exports = env => {
+  if (env && env.test) {
+    return testConfig;
+  } else {
+    return libConfig;
   }
 };
