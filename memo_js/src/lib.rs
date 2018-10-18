51cd0fb1469595bc0d1e9434b656ae72a3c31b5d
Antonio Scandurra <as-cii@github.com>
2018-10-17 21:57:46 -0600

    Implement AsyncIteratorToStream

--- memo_js/src/lib.rs
+++ memo_js/src/lib.rs
@@ -11,20 +11,27 @@ extern crate serde;
 extern crate wasm_bindgen;
 extern crate wasm_bindgen_futures;
 
-use futures::{Future, Stream};
+use futures::{Async, Future, Poll, Stream};
 use memo_core::*;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::cell::Cell;
 use std::char;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::marker::PhantomData;
 use std::path::Path;
 use std::rc::Rc;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::future_to_promise;
+use wasm_bindgen_futures::{future_to_promise, JsFuture};
 
 #[wasm_bindgen]
 struct WorkTree {}
+
+#[derive(Serialize, Deserialize)]
+struct AsyncResult<T> {
+    value: Option<T>,
+    done: bool,
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -34,39 +41,41 @@ extern "C" {
     fn next(this: &AsyncIterator) -> js_sys::Promise;
 }
 
-#[wasm_bindgen]
-struct StreamToIterator(Rc<Cell<Option<Box<Stream<Item = JsValue, Error = JsValue>>>>>);
-
-#[derive(Serialize, Deserialize)]
-struct AsyncResult<T> {
-    value: Option<T>,
-    done: bool,
+struct AsyncIteratorToStream<T, E> {
+    next_value: JsFuture,
+    iterator: AsyncIterator,
+    _phantom: PhantomData<(T, E)>,
 }
 
 #[wasm_bindgen]
-impl StreamToIterator {
-    pub fn next(&mut self) -> Option<js_sys::Promise> {
+struct StreamToAsyncIterator(Rc<Cell<Option<Box<Stream<Item = JsValue, Error = JsValue>>>>>);
 
-        let stream_rc = self.0.clone();
-        self.0.take().map(|stream| {
-            future_to_promise(stream.into_future().then(move |result| match result {
-                Ok((next, rest)) => {
-                    stream_rc.set(Some(rest));
-                    Ok(next.unwrap_or(
-                        JsValue::from_serde(&AsyncResult::<()> {
-                            value: None,
-                            done: true,
-                        })
-                        .unwrap(),
-                    ))
+impl<T, E> Stream for AsyncIteratorToStream<T, E>
+where
+    E: for<'de> Deserialize<'de>,
+    T: for<'de> Deserialize<'de>,
+{
+    type Item = T;
+    type Error = E;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+        match self.next_value.poll() {
+            Ok(Async::Ready(result)) => {
+                let result: AsyncResult<T> = result.into_serde().unwrap();
+                if result.done {
+                    Ok(Async::Ready(None))
+                } else {
+                    self.next_value = JsFuture::from(self.iterator.next());
+                    Ok(Async::Ready(result.value))
                 }
-                Err((error, _)) => Err(error),
-            }))
-        })
+            }
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(error) => Err(error.into_serde().unwrap()),
+        }
     }
 }
 
-impl StreamToIterator {
+impl StreamToAsyncIterator {
     fn new<E, S, T>(stream: S) -> Self
     where
         E: Serialize,
@@ -78,18 +87,30 @@ impl StreamToIterator {
                 JsValue::from_serde(&AsyncResult {
                     value: Some(value),
                     done: false,
-                })
-                .unwrap()
-            })
-            .map_err(|error| JsValue::from_serde(&error).unwrap());
+                }).unwrap()
+            }).map_err(|error| JsValue::from_serde(&error).unwrap());
 
-        StreamToIterator(Rc::new(Cell::new(Some(Box::new(js_value_stream)))))
+        StreamToAsyncIterator(Rc::new(Cell::new(Some(Box::new(js_value_stream)))))
     }
 }
 
 #[wasm_bindgen]
-impl WorkTree {
-    pub fn new() -> (Self, StreamToIterator) {
-        panic!()
+impl StreamToAsyncIterator {
+    pub fn next(&mut self) -> Option<js_sys::Promise> {
+        let stream_rc = self.0.clone();
+        self.0.take().map(|stream| {
+            future_to_promise(stream.into_future().then(move |result| match result {
+                Ok((next, rest)) => {
+                    stream_rc.set(Some(rest));
+                    Ok(next.unwrap_or(
+                        JsValue::from_serde(&AsyncResult::<()> {
+                            value: None,
+                            done: true,
+                        }).unwrap(),
+                    ))
+                }
+                Err((error, _)) => Err(error),
+            }))
+        })
     }
 }
